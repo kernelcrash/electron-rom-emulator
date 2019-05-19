951ce949d39ee38e6bceb055c3b2415a4ca365fa
Kernel Crash <kernel@kernelcrash.com>
2019-05-19 16:25:08 +1200

    switch to same adc thresholds as real joystick

--- main.h
+++ main.h
@@ -7,11 +7,12 @@
 //#define MAIN_THREAD_COMMANDS_MASK	(MAIN_THREAD_ROM_SWAP_COMMAND)
 
 // If your joystick can go from one extreme to the other then maybe use 0 to 255
-//#define ADC_LOW_THRESHOLD 0
-//#define ADC_HIGH_THRESHOLD 255
+#define ADC_LOW_THRESHOLD 0
+#define ADC_HIGH_THRESHOLD 255
 
-#define ADC_LOW_THRESHOLD 64
-#define ADC_HIGH_THRESHOLD 192
+// If your joystick does not go full swing from 0 to 255, you might need these 'jump thresholds' to help
+##define ADC_LOW_THRESHOLD 64
+##define ADC_HIGH_THRESHOLD 192
 
 #endif
 
